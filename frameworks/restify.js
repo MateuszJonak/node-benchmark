cad3480852d9bb70180fff7f3a0964685839394a
mateusz jonak <m.jonak@clearcode.cc>
2015-11-12 12:23:48 +0100

    move node server to app.js and fix restify

--- frameworks/restify.js
+++ frameworks/restify.js
@@ -5,9 +5,7 @@ var server = restify.createServer({
     version: '1.0.0'
 });
 
-// restify works only for 'http://localhost:3000/'.
-// There must be '/' on the end.
-server.get(/.*/, function (req, res, next) {
+server.get('/', function (req, res, next) {
     res.send(204);
     return next();
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,37 @@
 var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon'),
-  plumber = require('gulp-plumber'),
-  livereload = require('gulp-livereload');
+    nodemon = require('gulp-nodemon'),
+    plumber = require('gulp-plumber'),
+    livereload = require('gulp-livereload');
 var minimist = require('minimist');
 
 // OPTIONS
 var options = minimist(process.argv.slice(2));
-options.framework = options.framework || 'node';
+options.framework = options.framework;
 
 gulp.task('develop', function () {
-  var nodemonConf = {
-    script: './frameworks/' + options.framework + '.js',
-    ext: 'js',
-    stdout: false
-  };
 
-  livereload.listen();
-  nodemon(nodemonConf).on('readable', function () {
-    this.stdout.on('data', function (chunk) {
-      if(/^Express server listening on port/.test(chunk)){
-        livereload.changed(__dirname);
-      }
+    var nodemonConf = {
+        script: './app.js',
+        ext: 'js',
+        stdout: false
+    };
+
+    if (options.framework) {
+        nodemonConf.script = './frameworks/' + options.framework + '.js';
+    }
+
+    livereload.listen();
+    nodemon(nodemonConf).on('readable', function () {
+        this.stdout.on('data', function (chunk) {
+            if(/^Express server listening on port/.test(chunk)){
+                livereload.changed(__dirname);
+            }
+        });
+        this.stdout.pipe(process.stdout);
+        this.stderr.pipe(process.stderr);
     });
-    this.stdout.pipe(process.stdout);
-    this.stderr.pipe(process.stderr);
-  });
 });
 
 gulp.task('default', [
-  'develop'
+    'develop'
 ]);
